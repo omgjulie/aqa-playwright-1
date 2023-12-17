1b412edee47d15ae446ff15371d30b624e05c59a
Yuliia <yuliia.kovalchuk@gmail.com>
2023-12-17 18:16:41 +0200

    changed docker setup

--- playwright.config.github.js
+++ playwright.config.github.js
@@ -37,7 +37,10 @@ const config = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { open: "never" }], ["list"]],
+  reporter: [
+    ["html", { open: process.env.CI ? "never" : "on-failure" }],
+    ["list"],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
